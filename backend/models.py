062199489b3242962620a9d1814f4b07315ce347
dongzijing10 <111717790+dongzijing10@users.noreply.github.com>
2024-04-09 13:13:12 +0800

    function input

--- backend/models.py
+++ backend/models.py
@@ -1,30 +1,105 @@
 from pydantic import BaseModel
 from typing import Optional
+import datetime 
 
+# Optional[str] = None
 
 class tableInfo(BaseModel):
     tableName: str
     id: int|str
 
-
 class category(BaseModel):
     id: int|str
     cname: str
     explain: str
-    setup: str
-    update: str
+    setup: datetime.date
+    update: datetime.date
     pname:str
     pjob: str
     cadd: str
     city: str
-    
-class updateInfo(BaseModel):
-    tablename: str
+
+class customers(BaseModel):
+    cID: str
+    cname: str
+    pname: str
+    pjob: str
+    cadd: str
+    city: str
+    area: str
+    postcode: str
+    country: str
+    phone: str
+    fax: str
+
+class orderdetail(BaseModel):
+    orderid: int 
+    productid: int
+    num: int
+    remark: str
+
+class orders(BaseModel):
+    ID: int
+    orderdate: datetime.date
+    starttime: datetime.date
+    arrivaltime: datetime.date
+    confirmtime: datetime.date
+    cost: int
+    name: str
+    addr: str
+    city: str
+    area: str
+    postcode: str
+    paymethod: str
+    insurance: str
+
+class products(BaseModel):
+    ID: int
+    name: str
+    num: str
+    price: str
+    inventory: int
+    ordernum: int
+    reordernum: int
+    supplystate: bool
+
+class proinfo(BaseModel):
+    ID: int
+    productID: int 
+    prodate: datetime.date
+    expirationdate: datetime.date
+
+class shippers(BaseModel):
+    sID: int
+    sname: str
+    phone: str
+    tool: str
+
+class suppliers(BaseModel):
+    ID: int
+    name: str
+    pname: str
+    pjob: str
+    address: str
+    city: str
+    area: str
+    postcode: str
+    country: str
+    phone: str
+    fax: str
+    homepage: str
+
+class insertInfo(BaseModel):
+    tableName: str
+    id: int|str
     categoryInput: category   #嵌套定义类，将外面的类引用到类内部，在内部也可以嵌套，但其他的函数引用内部类时麻烦
-
-    # class customers():
-
-info:updateInfo
+    customersInput: customers
+    orderdetailInput: orderdetail
+    ordersInput: orders
+    productsInput: products
+    proinfoInput: proinfo
+    shippersInput: shippers
+    suppliersInput: suppliers
 
 
 
--- backend/sql_operation.py
+++ backend/sql_operation.py
@@ -46,15 +46,52 @@ def getInfo(info:models.tableInfo):
             return('row = %s' % (row,))
 
 
-def putInData(info:models.category):
-    cursor.execute('insert into category(ID,cname,explain) values(%d,%s,%s)',(9,info.cname,info.explain))
+def categoryInput(info:models.category):
+    cursor.execute('insert into category(ID,cname,explain) values(%d,%s,%s)',
+                   (info.id,info.cname,info.explain))
     connect.commit()
-    cursor.execute('select * from category ')    
-    for row in cursor:
-        return('row = %s' % (row,))
+    # cursor.execute('select * from category where ID = %d',info.id)    
+    # for row in cursor:
+    #     return('row = %s' % (row,))
 
-        # for row in cursor:
-        #     return('row = %s' % (row,))
+def customersInput(info:models.customers):#(cID,cname,pname,pjob,cadd,city,area,postcode,country,phone,fax) 
+    cursor.execute('insert into customers(cID,cname,pname,pjob,cadd,city,area,postcode,country,phone,fax) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',
+                   (info.cID,info.cname,info.pname,info.pjob,info.cadd,info.city,info.area,info.postcode,info.country,info.phone,info.fax))
+    connect.commit()
+
+def orderdetailInput(info:models.orderdetail):
+    cursor.execute('insert into orderdetail values(%d,%d,%d,%s)',
+                   (info.orderid,info.productid,info.num,info.remark))
+    connect.commit()
+
+def ordersInput(info:models.orders):
+    cursor.execute('insert into orders(ID,orderdate,starttime,arrivaltime,confirmtime,cost,name,addr,city,area,postcode,paymethon,insurance) values(%d,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%d)',
+                   (info.ID,info.orderdate,info.starttime,info.arrivaltime,info.confirmtime,info.cost,info.name,info.addr,info.city,info.area,info.postcode,info.paymethod,info.insurance))
+    connect.commit()
+
+def productsInput(info:models.products):
+    cursor.execute('insert into products(ID,name,num,price,inventory,ordernum,reordernum,supplystate) values(%d,%s,%s,%d,%d,%d,%d,%s)',
+                   (info.ID,info.price,info.inventory,info.ordernum,info.reordernum,info.supplystate))
+    connect.commit()
+
+def proinfoInput(info:models.proinfo):
+    cursor.execute('insert into proinfo(ID,productID,prodate,expiration) values(%d,%d,%s,%s)',
+                   (info.ID,info.productID,info.prodate,info.expirationdate))
+    connect.commit()
+
+def shippersInput(info:models.shippers):
+    cursor.execute('insert into shippers(sID,sname,phone,tool) values(%d,%s,%s,%s)',
+                   (info.sID,info.sname,info.phone,info.tool))
+    connect.commit()
+
+def suppliersInput(info:models.suppliers):
+    cursor.execute('insert into suppliers(ID,name,pname,pjob,address,city,area,postcode,country,phone,fax,homepage) values(%d,%s,%s)',
+                   (info.ID,info.name,info.pname,info.pjob,info.address,info.city,info.area,info.postcode,info.country,info.phone,info.fax,info.homepage))
+    connect.commit()
+
+def Order_update(info:models.orders):
+    cursor.executr('update orders set cost = %d where ID = %d',(info.cost,info.ID))
+    cursor.commit()
 
 # cursor.close()    
 # connect.close()     
